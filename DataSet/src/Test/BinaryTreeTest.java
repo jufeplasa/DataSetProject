2fb3850666771b2dcd0b7ed9917bd0fe1ada9f9f
Steban Rodriguez <amilcarshm@gmail.com>
2022-05-08 17:23:35 -0500

    Design of unit test Binary Tree

--- DataSet/src/Test/BinaryTreeTest.java
+++ DataSet/src/Test/BinaryTreeTest.java
@@ -13,12 +13,12 @@ public class BinaryTreeTest {
 
 	public void setupScene2() {
 		arbol = new BinaryTree<Person>();
-		Person person1 = new Person("Amilcar", "Rodriguez", 18, "05/08/2003", "photos/1.jpg", "Cali", 166.0,"2GOZO453");
+		Person person1 = new Person("Bairon", "Rodriguez", 18, "05/08/2003", "photos/1.jpg", "Cali", 166.0,"2GOZO453");
 		arbol.addPerson(person1);
 	}
 	public void setupScene3() {
 		arbol = new BinaryTree<Person>();
-		Person person1 = new Person("Amilcar", "Rodriguez", 18, "5/8/2003", "photos/1.jpg", "Cali", 166.0, "2GOZO453");
+		Person person1 = new Person("Bairon", "Rodriguez", 18, "5/8/2003", "photos/1.jpg", "Cali", 166.0, "2GOZO453");
 		Person person2 = new Person("Juan", "Felipe", 19, "25/9/2003", "photos/12.jpg", "Cali", 167.0, "2GOZO454");
 		Person person3 = new Person("Jorge", "Jojo", 19, "21/4/2003", "photos/24.jpg", "Cali", 162.0, "2GOZO455");
 		Person person4 = new Person("Diego", "Juan", 20, "1/4/2002", "photos/22.jpg", "Cali", 167.0, "2GOZO456");
@@ -34,7 +34,7 @@ public class BinaryTreeTest {
 		setupScene1();
 		arbol = new BinaryTree<Person>();
 		assertTrue(arbol.getRoot() == null);
-		Person person1 = new Person("Amilcar", "Rodriguez", 18, "05/08/2003", "photos/1.jpg", "Cali", 166.0,"2GOZO453");
+		Person person1 = new Person("Bairon", "Rodriguez", 18, "05/08/2003", "photos/1.jpg", "Cali", 166.0,"2GOZO453");
 		arbol.addPerson(person1);
 		assertTrue(arbol.getRoot() != null);
 	}
@@ -43,7 +43,7 @@ public class BinaryTreeTest {
 		setupScene2();
 		arbol = new BinaryTree<Person>();
 		Person person2 = new Person("Juan", "Felipe", 19, "25/9/2003", "photos/12.jpg", "Cali", 167.0, "2GOZO454");
-		Person person3 = new Person("Jorge", "Jojo", 19, "21/4/2003", "photos/24.jpg", "Cali", 162.0, "2GOZO455");
+		Person person3 = new Person("Amilcar", "Jojo", 19, "21/4/2003", "photos/24.jpg", "Cali", 162.0, "2GOZO455");
 		arbol.addPerson(person2);
 		arbol.addPerson(person3);
 	}
